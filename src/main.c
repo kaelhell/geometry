b690e0577fd803b66bf27601d2447bb067bf3cd9
Горюнова Диана Николаевна <is841s03@bwp02.csc.local>
2019-05-17 14:00:40 +0700

    Change hardcode path to argv

--- src/main.c
+++ src/main.c
@@ -5,10 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	if(argc != 2) {
+		printf("Usage: geometry <file>\n");
+		return 0;
+	}
     FILE* myfile;
-    myfile = fopen("./example/myfile.txt", "r");
+    myfile = fopen(argv[1], "r");
     if (myfile == NULL) {
         printf("No file\n");
         return 1;
